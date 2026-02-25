1d1f0bd8941e2b464fda8cea4e2ba643717c1b31
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-02-25 18:12:24 +0545

    calculate marks

---
+++ python/marks.py
@@ -0,0 +1,15 @@
+mark1 = int(input("Enter a marks of 1st subject: "))
+mark2 = int(input("Enter a marks of 2nd subject: "))
+mark3 = int(input("Enter a marks of 3rd subject: "))
+
+total = mark1 + mark2 + mark3
+
+avg = total/3
+print("---------------------------")
+print("Python:", mark1)
+print("Logic:", mark2)
+print("Java:",mark3)
+
+print("Total marks:", total)
+print("Average marks:", avg)
+print("---------------------------")

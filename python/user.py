c0b0836a85917f6aa2c821c68633cc6b0c72adc7
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-02-25 18:55:09 +0545

    greatest number among 3 number

---
+++ python/user.py
@@ -0,0 +1,12 @@
+
+
+a = int(input("Enter a 1st number: "))
+b = int(input("Enter a 2nd number: "))
+c = int(input("Enter a 3rd number: "))
+
+if a>b and a>c:
+    print(a ,"is greatest number")
+elif b>a and b>c:
+    print(b ,"is greatest number")
+else :
+    print(c ,"is greatest number")

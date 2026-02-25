3f0dab943f35ccdff487f14ccf71fa5026088c51
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-02-25 18:52:45 +0545

    quadratic equation

---
+++ python/quadratic.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a value of a: "))
+b = int(input("Enter a value of b: "))
+c = int(input("Enter a value of c: "))
+
+x1 =(-b +(b**2-4*a*c)**(1/2))/2*a
+x2 =(-b - (b**2-4*a*c)**(1/2))/2*a
+
+print(x1, x2)

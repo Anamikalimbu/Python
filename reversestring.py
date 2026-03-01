5bbf127630f1403b3ba56368a970ee7b22934091
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-03-01 20:56:20 +0545

    reverse string

---
+++ reversestring.py
@@ -0,0 +1,6 @@
+#write a program that asks user to enter a string and print it in reverse order.
+
+string = input("Enter a string: ")
+print("The reverse of the string is:", string[::-1])
+
+#::-1 is use to reverse the order of the string. It starts from the end of the string and goes to the beginning.

7d4f7109c38a3de5bb76e0ad7e26f6648908604a
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-03-16 19:40:00 +0545

    Add functions for greeting and addition

---
+++ Functions/1func.py
@@ -0,0 +1,15 @@
+# functions
+# Built-in functions
+print("Hello, World!")
+
+# User-defined functions
+def greet(name):
+    print(f"Hello, {name}!")
+greet("Anamika")
+
+
+# Function with return value
+def add_numbers(a, b):
+    return a + b
+result = add_numbers(5, 3)
+print(result)

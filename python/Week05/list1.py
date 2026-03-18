70251fd1c4cd405aa11ddcc9bed1ccc9c3fbd553
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-03-18 19:05:33 +0545

    print multiples of 3 and 2 from list
    Modified the loop to print even numbers and multiples of three separately.

---
+++ python/Week05/list1.py
@@ -0,0 +1,14 @@
+# a=[2,3,6,8,9,12,15,18,24,22,33,112]
+
+# for num in a:
+#     if num % 2 == 0 and num % 3 == 0:
+#         print(num)
+    
+
+a=[2,3,6,8,9,12,15,18,24,22,33,112]
+
+for num in a:
+    if num % 2 == 0:
+        print(num)
+    elif num % 3 == 0:
+        print(num)

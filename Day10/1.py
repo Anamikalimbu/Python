dd61b29407d8e4b32d7d467d87461fb1588e8fee
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-03-23 19:20:12 +0545

    list

---
+++ Day10/1.py
@@ -0,0 +1,11 @@
+list = [1,2,3,4,5,6,7,8,9]
+print(list)
+# Print first element
+print("First element: ",list[0])
+# Print last element
+print("Last element: ",list[-1])
+# Print middle element
+print("Middle element: ",list[4])
+# Replace the 3rd element (index 2) with a new value
+list[2]=10
+print("Updated list:", list)

5bd1d1369e08a4f8d3c722c35f0b8c3683458ac9
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-03-25 18:51:27 +0545

    set

---
+++ python/week6/Workshop1.py
@@ -0,0 +1,7 @@
+original_list = ['a','a','b','c','c','d','c','e','d','e']
+unique_elements = set(original_list)
+result_list = sorted(unique_elements)
+
+# Output 
+print(f"Original:  {original_list}")
+print(f"Result : {result_list}")
